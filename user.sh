67453335c2bf757e27ac16cb014a48676c4a4e28
Pedro Trugillo Garcia <155574001+PedroGrilloXD@users.noreply.github.com>
2024-01-22 20:59:12 -0300

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "max_execution_time = 300" >> user.ini
+echo "max_input_time = 90" >> user.ini
+echo "memory_limit = 512M" >> user.ini
+echo "post_max_size = 512M" >> user.ini
+echo "upload_max_filesize = 512M" >> user.ini
+echo "max_input_vars = 2000" >> user.ini
+
+rm "$0"
